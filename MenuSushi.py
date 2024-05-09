802ba0e70f584e7107bdd6634f2faf9d5dcbc4a5
Andrexitico <169111701+Andrexitico@users.noreply.github.com>
2024-05-08 22:17:23 -0400

    Add files via upload

---
+++ MenuSushi.py
@@ -0,0 +1,10 @@
+while True:
+    print("Bienvenido al menu de sushi: ")
+    print("1. Pikachu Roll $4500")
+    print("2. Otaku Roll $5000")
+    print("3. Pulpo Venenoso Roll $5200")
+    print("4. Anguila Electrica Roll $4800")
+    break
+
+
+
